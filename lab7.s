7323c1f5012ec55463e7d747e1c59bfeb2b758b8
Felix A Wilking <felixwil@timberlake.cse.buffalo.edu>
2023-04-11 13:31:30 -0400

    routine skeleton

--- lab7.s
+++ lab7.s
@@ -2,18 +2,100 @@
 
 	.global lab7
 
+	.global uart_init
+	.global output_character
+	.global output_string
+
+begincolorescape: .string "\033[3", 0
+endcolorescape:   .string ";1;1m", 0
 
 lab7:
 	PUSH {lr}   ; Store lr to stack
 
+	BL uart_init
 		; Your code is placed here.
  		; Sample test code starts here
 
-
+mainloop:
+	B mainloop
 		; Sample test code ends here
 
 
 	POP {lr}	  ; Restore lr from stack
+	mov pc, lr
+
+
+; generates a random integer
+randInt:
+	PUSH {lr, r4-r11}
+	POP  {lr, r4-r11}	  ; Restore lr from stack
+	mov pc, lr
+
+; generates a random color value 1..5
+randomColor:
+	PUSH {lr, r4-r11}
+	POP  {lr, r4-r11}	  ; Restore lr from stack
+	mov pc, lr
+
+; apply one time unit of velocity
+moveBall:
+	PUSH {lr, r4-r11}
+	POP  {lr, r4-r11}	  ; Restore lr from stack
+	mov pc, lr
+
+; return true/false if ball is touching top, left, or right sides.
+btouchWAD:
+	PUSH {lr, r4-r11}
+	POP  {lr, r4-r11}	  ; Restore lr from stack
+	mov pc, lr
+
+; return true/false if ball is touching bottom
+btouchS:
+	PUSH {lr, r4-r11}
+	POP  {lr, r4-r11}	  ; Restore lr from stack
+	mov pc, lr
+
+; return true/false if ball is touching paddle
+btouchPaddle:
+	PUSH {lr, r4-r11}
+	POP  {lr, r4-r11}	  ; Restore lr from stack
+	mov pc, lr
+
+; return true/false if ball is touching a brick
+btouchBrick:
+	PUSH {lr, r4-r11}
+	POP  {lr, r4-r11}	  ; Restore lr from stack
+	mov pc, lr
+
+; change ball velocity
+bounceBall:
+	PUSH {lr, r4-r11}
+	POP  {lr, r4-r11}	  ; Restore lr from stack
+	mov pc, lr
+
+; update paddle position
+movePaddle:
+	PUSH {lr, r4-r11}
+	POP  {lr, r4-r11}	  ; Restore lr from stack
+	mov pc, lr
+
+; set putty terminal color
+setColor:
+	PUSH {lr, r4-r11}
+
+	POP  {lr, r4-r11}	  ; Restore lr from stack
+	mov pc, lr
+
+; reset terminal color
+resetColor:
+	PUSH {lr, r4-r11}
+	POP  {lr, r4-r11}	  ; Restore lr from stack
+	mov pc, lr
+
+; clears the board and resets all the bricks, the ball, and the paddle
+levelClear:
+	PUSH {lr, r4-r11}
+	POP  {lr, r4-r11}	  ; Restore lr from stack
 	mov pc, lr
 
 	.end
--- lab7library.s
+++ lab7library.s
@@ -13,3 +13,114 @@
         .global read_from_keypad
         .global string2int
         .global int2string
+
+uart_init:
+        PUSH {lr, r0, r1}  ; Store register lr on stack
+
+        ; copied comments from lab3wrapper.c obviously
+
+        ;/* Provide clock to UART0  */
+	; (*((volatile uint32_t *)(0x400FE618))) = 1;
+        MOV r0, #0xe618
+        MOVT r0, #0x400f
+        MOV r1, #1
+        STRW r1, [r0]
+
+	;/* Enable clock to PortA  */
+	; (*((volatile uint32_t *)(0x400FE608))) = 1;
+        MOV r0, #0xe608
+        MOVT r0, #0x400f
+        MOV r1, #1
+        STRW r1, [r0]
+
+	;/* Disable UART0 Control  */
+	; (*((volatile uint32_t *)(0x4000C030))) = 0;
+        MOV r0, #0xc030
+        MOVT r0, #0x4000
+        MOV r1, #0
+        STRW r1, [r0]
+
+	;/* Set UART0_IBRD_R for 115,200 baud */
+	; (*((volatile uint32_t *)(0x4000C024))) = 8;
+	MOV r0, #0xc024
+	MOVT r0, #0x4000
+	MOV r1, #8
+	STRW r1, [r0]
+
+	;/* Set UART0_FBRD_R for 115,200 baud */
+	; (*((volatile uint32_t *)(0x4000C028))) = 44;
+        MOV r0, #0xc028
+        MOVT r0, #0x4000
+        MOV r1, #44
+        STRW r1, [r0]
+
+	;/* Use System Clock */
+	; (*((volatile uint32_t *)(0x4000CFC8))) = 0;
+        MOV r0, #0xcfc8
+        MOVT r0, #0x4000
+        MOV r1, #0
+        STRW r1, [r0]
+
+	;/* Use 8-bit word length, 1 stop bit, no parity */
+	; (*((volatile uint32_t *)(0x4000C02C))) = 0x60;
+        MOV r0, #0xc02c
+        MOVT r0, #0x4000
+        MOV r1, #0x60
+        STRW r1, [r0]
+
+	;/* Enable UART0 Control  */
+	; (*((volatile uint32_t *)(0x4000C030))) = 0x301;
+        MOV r0, #0xc030
+        MOVT r0, #0x4000
+        MOV r1, #0x0301
+        STRW r1, [r0]
+
+	;/* Make PA0 and PA1 as Digital Ports  */
+	; (*((volatile uint32_t *)(0x4000451C))) |= 0x03;
+        MOV r0, #0x451c
+        MOVT r0, #0x4000
+        LDRB r1, [r0]
+        ORR r1, r1, #0x03
+        STRW r1, [r0]
+
+	;/* Change PA0,PA1 to Use an Alternate Function  */
+	; (*((volatile uint32_t *)(0x40004420))) |= 0x03;
+        MOV r0, #0x4420
+        MOVT r0, #0x4000
+        LDRB r1, [r0]
+        ORR r1, r1, #0x03
+        STRW r1, [r0]
+
+	;/* Configure PA0 and PA1 for UART  */
+	; (*((volatile uint32_t *)(0x4000452C))) |= 0x11;
+        MOV r0, #0x452c
+        MOVT r0, #0x4000
+        LDRB r1, [r0]
+        ORR r1, r1, #0x11
+        STRW r1, [r0]
+
+        POP {lr, r0, r1}
+        mov pc, lr
+
+
+output_string:
+        PUSH {lr}   ; Store register lr on stack
+
+        MOV r1, r0 ; copying the pointer
+
+outputstringloop:
+        LDRB r0, [r1] ; getting the char at the pointer (ptr[0])
+
+        CMP r0, #0 ; if the char is null char, exit
+        BEQ exitoutputstring
+
+        BL output_character ; call the output char function to transmit r0 over uart
+
+        ADD r1, r1, #1 ; increment char pointer
+
+        B outputstringloop ; go back up
+
+exitoutputstring:
+
+        POP {lr}
+        mov pc, lr
