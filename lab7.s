3cc96760c1c847f52528ed0eb42cf3bd662b6e61
Felix A Wilking <felixwil@timberlake.cse.buffalo.edu>
2023-04-18 01:30:48 -0400

    Added a bunch of btouch routines

--- lab7.s
+++ lab7.s
@@ -5,9 +5,14 @@
 	.global uart_init
 	.global output_character
 	.global output_string
+    .global int2string
+	
+	.global uart_interrupt_init
+    .global gpio_interrupt_init
+    .global timer_interrupt_init
 
-beginColorEscape: 	.string 27, "[3", 0
 beginBackgroundEscape: .string 27, "[4", 0
+beginColorEscape: 	.string 27, "[3", 0
 endColorEscape:   	.string ";1;1m", 0
 resetColorString:   .string 27, "[0m", 0
 brickState:  		.word 0x0
@@ -16,15 +21,16 @@ yDelta: 			.byte 0x00
 score: 				.word 0x0
 ballxPosition:  	.byte 0x0B
 ballyPosition:  	.byte 0x08
-paddlePosition: 	.byte 0x09
+paddlePos: 			.byte 0x09
 ballColor:  		.byte 0x00
 lives:  			.byte 0x04
 level:  			.byte 0x01
 pauseState:  		.byte 0x00
 scoreString:		.string "Score: ", 0
-topBottomBorder:	.string "+---------------------+"
+topBottomBorder:	.string "+---------------------+", 0
 
 ; Pointers to memory locations
+ptr_to_beginBackgroundEscape	.word beginBackgroundEscape
 ptr_to_beginColorEscape:		.word beginColorEscape
 ptr_to_endColorEscape:			.word endColorEscape
 ptr_to_brickState:				.word brickState
@@ -33,7 +39,7 @@ ptr_to_yDelta:					.word yDelta
 ptr_to_score:					.word score
 ptr_to_ballxPosition:			.word ballxPosition
 ptr_to_ballyPosition:			.word ballyPosition
-ptr_to_paddlePosition:			.word paddlePosition
+ptr_to_paddlePos:				.word paddlePos
 ptr_to_ballColor:				.word ballColor
 ptr_to_lives:  					.word lives
 ptr_to_level:					.word level
@@ -46,14 +52,15 @@ lab7:
 	PUSH {lr}   ; Store lr to stack
 
 	BL uart_init
-
 	MOV r0, #0xc
 	BL output_character
 
-	MOV r0, #0x2
-	BL output_character
+	BL printBoard
 
-	MOV r1, #0x3
+	MOV r0, #0x0
+	;BL output_character
+
+	MOV r1, #0
 	BL setCursorxy
 
 	MOV r0, #2 ; set color to yellow
@@ -78,6 +85,67 @@ mainloop:
 	POP {lr}	  ; Restore lr from stack
 	mov pc, lr
 
+Timer_Handler:
+    ; Save the registers
+    PUSH {lr, r4-r11}
+
+	; Clear the interrupt, using load -> or -> store to not overwrite other data
+    MOV  r11, #0x0024
+    MOVT r11, #0x4003			; Address for interrupt
+    LDRB r4, [r11]          	; Load interrupt value
+    ORR r4, r4, #1          	; Set bit 0 to 1
+	STRB r4, [r11]				; Store back to clear interrupt
+
+    ; Update position based on direction stored in current_direction and switch_speed
+    # ldr r7, ptr_to_yposition
+    # ldrbs r9, [r7]
+    # ldr r8, ptr_to_xposition
+    # ldrbs r10, [r8]
+
+	# cmp r9, #0xFF
+	# BEQ game_over_handler
+
+	# cmp r9, #20
+	# BEQ game_over_handler
+
+    # ldr r5, ptr_to_current_direction
+    # LDRB r6, [r5]
+    # cmp r6, #0x77 ; W
+    # IT EQ
+    # ADDEQ r9, r9, #-1 ; y += 1
+    # ;ADDEQ r10, r10, #-1 ; y += 1
+    # cmp r6, #0x61 ; A
+    # IT EQ
+    # ADDEQ r10, r10, #-1 ; x -= 1
+    # cmp r6, #0x73 ; S
+    # IT EQ
+    # ADDEQ r9, r9, #1 ; y -= 1
+    # cmp r6, #0x64 ; D
+	# IT EQ
+	# ADDEQ r10, r10, #1 ; x += 1
+
+    # cmp r10, #0
+	# BEQ game_over_handler
+
+	# cmp r10, #21
+	# BEQ game_over_handler
+
+	# strb r9, [r7]
+	# strb r10, [r8]
+
+	# ldr r7, ptr_to_movement_counter
+	# LDRB r8, [r7]
+	# ADD r8, r8, #1
+	# STRB r8, [r7]
+
+    # ; Call display board to referesh the screen
+    # BL displayboard
+
+    ; Restore the registers
+    POP {lr, r4-r11}
+
+	BX lr       	; Return
+
 escapeSequence:
 	PUSH {lr}
 
@@ -92,24 +160,30 @@ setCursorxy:
 	PUSH{lr, r4-r11}
 
 	MOV r4, r0 ; x C 0x43
+	ADD r4, r4, #0x30
 	MOV r5, r1 ; y B 0x42
+	ADD r5, r5, #0x31
 	BL escapeSequence
 	MOV r0, #0x48
 	BL output_character
 
+	CMP r4, #0x30
+	BEQ noxshift
 	BL escapeSequence
-	ADD r4, r4, #0x30
 	MOV r0, r4
 	BL output_character
 	MOV r0, #0x43 ; column shift for x
 	BL output_character
+noxshift:
 
+	CMP r5, #0x30
+	BEQ noyshift
 	BL escapeSequence
-	ADD r5, r5, #0x30
 	MOV r0, r5
 	BL output_character
 	MOV r0, #0x42 ; row shift for y
 	BL output_character
+noyshift:
 
 	POP {lr, r4-r11}
 	mov pc, lr
@@ -124,14 +198,14 @@ printBoard:
 	BL setCursorxy				; Set cursor to correct position
 	LDR r0, ptr_to_scoreString
 	BL output_string			; Print "Score: "
-	LDR r0, ptr_to_score
-	LDRW r0, [r0]				; Load score
-	BL int2string				; Convert score to string
-	BL output_string			; Output the score
+	; LDR r0, ptr_to_score
+	;LDW r0, [r0]				; Load score
+	;BL int2string				; Convert score to string
+	; BL output_string			; Output the score
 
 	; Print the upper boarder
 	MOV r0, #0
-	MOV r0, #0
+	MOV r1, #0
 	BL setCursorxy				; Set cursor to beginning of second line
 	LDR r0, ptr_to_topBottomBorder
 	BL output_string			; Print the border
@@ -141,12 +215,12 @@ printBoard:
 	MOV r5, #0x7C				; Vertical line
 printWalls:
 	MOV r0, #0
-	MOV r0, r4
+	MOV r1, r4
 	BL setCursorxy				; Set the cursor position, (0, row)
 	MOV r0, r5
 	BL output_character			; Set character to print to be "|", then output it
 	MOV r0, #22
-	MOV r0, r4
+	MOV r1, r4
 	BL setCursorxy				; Set the cursor position, (22, row)
 	MOV r0, r5
 	BL output_character			; Set character to print to be "|", then output it
@@ -179,11 +253,144 @@ displayBricks:
 
 	; Jump back to loop
 
-; return true/false if ball is touching a brick
+; needs to be called before any usage of the btouch methods
+setupForBounceChecks:
+	PUSH {lr}
+	
+	ldr r2, ptr_to_ballxPosition
+	ldr r3, ptr_to_ballyPosition
+	ldr r4, ptr_to_paddlePos
+
+	POP {pc}
+
+; return true/false if ball is on a brick
+; x and y values in r2, r3, value returned in r1
 btouchBrick:
-	PUSH {lr, r4-r11}
-	POP  {lr, r4-r11}	  ; Restore lr from stack
-	mov pc, lr
+	PUSH {lr, r5}
+	; return false if y is greater than 6 or equal to 1
+	CMP r3, #1 ; y == 1
+	BEQ btouchBrickfalse ; return false
+	CMP r3, #6 ; y > 6
+	BGT btouchBrickfalse ; return false
+	; shift brickState by x*7 places
+	ldrw r1, ptr_to_brickState
+	MOV r5, #7
+	MUL r5, r2, r5 ; r5 = x*7
+	LSR r1, r1, r5 ; brickstate >>= r5
+	AND r1, r1, #1 ; brickstate & 1
+
+	POP  {pc, r5}
+
+btouchBrickfalse:
+
+	MOV r1, #0
+	POP  {pc, r5}
+
+
+; return true/false if ball is on a side
+; x and y values in r2, r3, value returned in r1
+btouchSide:
+	PUSH {lr}
+	; return true if x is 0 or greater than 21
+	CMP r2, #0
+	BEQ btouchsidetrue
+	CMP r2, #21
+	BGT btouchsidetrue
+
+	MOV r1, #0 	  ; false
+	POP  {pc}	  ; Restore lr from stack
+
+btouchSidetrue:
+	MOV r1, #1
+	POP  {pc}	  ; Restore lr from stack
+
+; return true/false if ball is on the top
+; x and y values in r2, r3, value returned in r1
+btouchtop:
+	PUSH {lr}
+	CMP r3, #0
+	ITE LE
+	MOVLE r1, #1
+	MOVGT r1, #0
+	POP  {pc}
+
+; return true/false if ball is on the bottom
+; x and y values in r2, r3, value returned in r1
+btouchbot:
+	PUSH {lr}
+	CMP r3, #17
+	ITE GE
+	MOVGE r1, #1
+	MOVLT r1, #0
+	POP  {pc}
+
+; return -1 if ball not on paddle, else the position on the paddle
+; which it is touching
+; x and y values and paddlepos in r2, r3, r4 value returned in r1
+btouchPaddle:
+	PUSH {lr}
+	; return true if y position = 16 and x < paddlepos or x > paddlepos+4
+	CMP r3, #16 ; y < 16
+	BLT btouchPaddlefalse ; return false
+
+	CMP r2, r4 ; x < paddlepos
+	BLT btouchPaddlefalse ; return false
+
+	ADD r4, r4, #4
+	CMP r2, r4 ; x > paddlepos + 4
+	BGT btouchPaddlefalse ; return false
+
+	SUB r1, r2, r4
+	POP  {pc} ; return true
+
+btouchPaddlefalse:
+	MOV r1, #-1
+	POP  {pc}
+
+; needs btouch setup to be called before
+updateBallDeltaForPaddleBounce:
+	PUSH {lr}
+	BL btouchPaddle
+	CMP r1, #-1
+	BEQ exitUpdateBallDelta
+
+	ldr r2, ptr_to_xDelta
+	ldr r3, ptr_to_yDelta
+
+	CMP r1, #2
+	ITTT EQ
+	MOVEQ r2, #0
+	MOVEQ r3, #-1
+	BEQ exitUpdateBallDelta
+
+	CMP r2, #0
+	ITE LT
+	MOVLT r2, #-1
+	MOVGE r2, #1
+
+	CMP r1, #3
+	IT EQ
+	MOVEQ r1, #1
+	CMP r1, #4
+	IT EQ
+	MOVEQ r1, #0
+
+	CMP r1, #1
+	ITT EQ
+	MOVEQ r3, #-2
+	BEQ exitUpdateBallDelta
+
+	CMP r1, #0
+	ITT EQ
+	MOVEQ r3, #-1
+	BEQ exitUpdateBallDelta
+
+exitUpdateBallDelta:
+	str r2, ptr_to_xDelta
+	str r3, ptr_to_yDelta
+
+exitBallDeltaCheck:
+	POP {pc}
 
 ; change ball velocity
 bounceBall:
--- lab7library.s
+++ lab7library.s
@@ -14,6 +14,10 @@
         .global string2int
         .global int2string
 
+	.global uart_interrupt_init
+        .global gpio_interrupt_init
+        .global timer_interrupt_init
+
 uart_init:
         PUSH {lr, r0, r1}  ; Store register lr on stack
 
@@ -102,6 +106,154 @@ uart_init:
         POP {lr, r0, r1}
         mov pc, lr
 
+uart_interrupt_init:
+        PUSH {lr, r4-r11}          ; store regs
+        ; Configure UART for interrupts
+        
+        MOV  r11, #0xc038
+        MOVT r11, #0x4000          ; setting the address
+        LDRW r4, [r11]             ; loading the data into r4
+        
+        ORR r4, r4, #16             ; r4 |= #8
+        
+        MOV  r11, #0xc038
+        MOVT r11, #0x4000          ; setting the address
+        STRW r4, [r11]             ; storing the data from r4
+        ; Set processor to allow for interrupts from UART0
+        
+        MOV  r11, #0xe100
+        MOVT r11, #0xe000          ; setting the address
+        LDRW r4, [r11]             ; loading the data into r4
+        
+        ORR r4, r4, #32            ; r4 |= #16
+        
+        MOV  r11, #0xe100
+        MOVT r11, #0xe000          ; setting the address
+        STRW r4, [r11]             ; storing the data from r4
+        
+        POP {lr, r4-r11}           ; restore saved regs
+        MOV pc, lr                 ; return to source call
+
+gpio_interrupt_init:
+        PUSH {lr, r4-r11}          ; store regs
+        
+        ; Set interrupt to be edge sensitive
+        MOV  r11, #0x5404
+        MOVT r11, #0x4002          ; setting the address
+        LDRW r4, [r11]             ; loading the data into r4
+        
+        AND r4, r4, #0xfe          ; r4 &= #0xf7
+        
+        MOV  r11, #0x5404
+        MOVT r11, #0x4002          ; setting the address
+        STRW r4, [r11]             ; storing the data from r4
+        
+        ; Set trigger for interrupt to be single edge
+        MOV  r11, #0x5408
+        MOVT r11, #0x4002          ; setting the address
+        LDRW r4, [r11]             ; loading the data into r4
+        
+        AND r4, r4, #0xfe          ; r4 &= #0xf7
+        
+        MOV  r11, #0x5408
+        MOVT r11, #0x4002          ; setting the address
+        STRW r4, [r11]             ; storing the data from r4
+        
+        ; Set the falling edge to be the trigger (triggers on press, not release)
+        MOV  r11, #0x540c
+        MOVT r11, #0x4002          ; setting the address
+        LDRW r4, [r11]             ; loading the data into r4
+        
+        AND r4, r4, #0xfe          ; r4 &= #0xf7
+        
+        MOV  r11, #0x540c
+        MOVT r11, #0x4002          ; setting the address
+        STRW r4, [r11]             ; storing the data from r4
+        
+        ; Enable the the interrupt
+        MOV  r11, #0x5410
+        MOVT r11, #0x4002          ; setting the address
+        LDRW r4, [r11]             ; loading the data into r4
+        
+        ORR r4, r4, #16          ; r4 |= #0x08
+        
+        MOV  r11, #0x5410
+        MOVT r11, #0x4002          ; setting the address
+        STRW r4, [r11]             ; storing the data from r4
+        
+        ; Set processor to allow interrupts from GPIO port F 
+        MOV  r11, #0xe100
+        MOVT r11, #0xe000          ; setting the address
+        LDRW r4, [r11]             ; loading the data into r4
+        
+        ORR r4, r4, #0x40000000    ; r4 |= #0x20000000
+        
+        MOV  r11, #0xe100
+        MOVT r11, #0xe000          ; setting the address
+        STRW r4, [r11]             ; storing the data from r4
+
+        
+        POP {lr, r4-r11}           ; restore saved regs
+        MOV pc, lr                 ; return to source call             ; return to source call
+
+timer_interrupt_init:
+        ; Connect clock to timer
+        MOV r11, #0xE604
+        MOVT r11, #0x400F   ; load address
+        LDRW r4, [r11]      ; load value
+        ORR r4, r4, #1      ; write 1 to bit 0
+        STRW r4, [r11]      ; store back
+
+        ; Disable timer
+        MOV r11, #0x000C
+        MOVT r11, #0x4003   ; load address
+        LDRW r4, [r11]      ; load value
+        AND r4, r4, #0xFE      ; write 1 to bit 0
+        STRW r4, [r11]      ; store back
+
+        ; Put timer into 32-bit mode
+        MOV r11, #0x0000
+        MOVT r11, #0x4003   ; load address
+        LDRW r4, [r11]      ; load value
+        AND r4, r4, #0xF8   ; write 0 to first 3 bits
+        STRW r4, [r11]      ; write back
+
+        ; Put timer into periodic mode
+        MOV r11, #0x0004
+        MOVT r11, #0x4003   ; load address
+        LDRW r4, [r11]      ; load value
+        AND r4, r4, #0xFE      ; write 2 to first two bits
+        ORR r4, r4, #2      ; write 2 to first two bits
+        STRW r4, [r11]      ; write back
+
+        ; Set up interval period
+        MOV r11, #0x0028
+        MOVT r11, #0x4003   ; load address
+        MOV r4, #0x2400
+        MOVT r4, #0x00F4    ; load frequency
+        STRW r4, [r11]      ; store frequency
+
+        ; Enable timer to interrupt processor
+        MOV r11, #0x0018
+        MOVT r11, #0x4003   ; load address
+        LDRW r4, [r11]      ; load value
+        ORR r4, r4, #1      ; write 1 to bit 0
+        STRW r4, [r11]      ; write back
+
+        ; Configure processor to allow timer interrupts
+        MOV r11, #0xE100
+        MOVT r11, #0xE000   ; load address
+        LDR r4, [r11]       ; load value
+        ORR r4, r4, #1 << 19 ; 0x80000; write 1 to bit 19
+        STRW r4, [r11]       ; write back
+
+        ; Enable timer
+        MOV r11, #0x000C
+        MOVT r11, #0x4003  ; load address
+        LDRW r4, [r11]      ; load value
+        ORR r4, r4, #1      ; write 1 to bit 0
+        STRW r4, [r11]
+
 output_character:
         PUSH {lr, r4-r11}   ; Store register lr on stack
 
@@ -141,3 +293,47 @@ exitoutputstring:
 
         POP {lr, r4-r11}
         mov pc, lr
+
+        int2string:
+        PUSH {lr, r4-r6}      ; Store register lr on stack
+
+	; r0: int, r1: char*
+
+        MOV r5, #10
+        MOV r6, r0
+        MOV r2, #0
+
+charactercounterloop:
+
+        ADD r2, r2, #1;  increment i
+        SDIV r6, r6, r5; number //= 10 (floor divide by 10)
+
+        CMP r6, #0;
+        BGT charactercounterloop; return to the top
+
+        ADD r1, r1, r2
+        SUB r1, r1, #1
+        STRB r6, [r1, #1]
+
+nextplace:
+
+        MOV r4, r0
+        sdiv r2, r4, r5
+        mul r3, r2, r5 ; r4 %= 10
+        sub r4, r4, r3
+
+        ; converting r4 from 0-9 to an ascii char '0'-'9'
+	; 0x30 == '0' etc
+        ADD r4, r4, #0x30
+        STRB r4, [r1] ; storing into the buffer
+        SUB r1, r1, #1 ; decrementing the pointer (because the digits are read in reverse)
+        MOV r11, #10
+        SDIV r0, r0, r11
+
+        CMP r0, #0 ; same loop condition as above.  (r0 // 10) == 0 means we've exhausted all digits
+        BNE nextplace  ; go back up
+
+        POP {lr, r4-r6}
+        mov pc, lr
+
+        .end
