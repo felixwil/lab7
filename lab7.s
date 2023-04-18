ae59507146af29f9bdfa227ceaecad8367a45cc9
Felix A Wilking <felixwil@timberlake.cse.buffalo.edu>
2023-04-18 12:59:37 -0400

    compiles now

--- lab7.s
+++ lab7.s
@@ -11,6 +11,8 @@
     .global gpio_interrupt_init
     .global timer_interrupt_init
 
+xescapeStringBuffer: .string 0,0,0
+yescapeStringBuffer: .string 0,0,0
 beginBackgroundEscape: .string 27, "[4", 0
 beginColorEscape: 	.string 27, "[3", 0
 endColorEscape:   	.string ";1;1m", 0
@@ -30,7 +32,10 @@ scoreString:		.string "Score: ", 0
 topBottomBorder:	.string "+---------------------+", 0
 
 ; Pointers to memory locations
-ptr_to_beginBackgroundEscape	.word beginBackgroundEscape
+
+ptr_to_xescapeStringBuffer: 	.word xescapeStringBuffer
+ptr_to_yescapeStringBuffer: 	.word yescapeStringBuffer
+ptr_to_beginBackgroundEscape:	.word beginBackgroundEscape
 ptr_to_beginColorEscape:		.word beginColorEscape
 ptr_to_endColorEscape:			.word endColorEscape
 ptr_to_brickState:				.word brickState
@@ -96,55 +101,86 @@ Timer_Handler:
     ORR r4, r4, #1          	; Set bit 0 to 1
 	STRB r4, [r11]				; Store back to clear interrupt
 
+	; Calculate the next position of the ball
+	; Load x and y positions
+	; Add x and y delta to their current position
+	LDR r5, ptr_to_ballxPosition
+	LDRB r5, [r5]
+	LDR r6, ptr_to_ballyPosition
+	LDRB r6, [r6]					; Load current x and y position into r5 and r6 (don't change these registers, need them later)
+	LDR r7, ptr_to_xDelta
+	LDRB r7, [r7]
+	LDR r8, ptr_to_yDelta
+	LDRB r8, [r8]					; Load current x and y deltas into r7 and r8 (change these to hold potential new position)
+	ADD r7, r7, r5
+	ADD r8, r8, r6					; Add x and y postions to their respective deltas and store back into r7 and r8
+	MOV r4, #0						; Register to track if any touches happen
+
+	; See if ball hits a wall
+	; Call btouchSide, if r = 1, update deltas
+	MOV r2, r7
+	MOV r3, r8						; Pass potential positions to touch functions
+	BL btouchSide
+	CMP r1, #1
+	BNE checkRoof					; If no touch, continue to next check
+
+
+checkRoof:
+	; See if ball hits roof
+	; Call btouchTop, if r = 1, update deltas
+
+checkBrick:
+	; See if ball hits brick
+	; Call btouchBrick, if r1 = 1, update deltas, NEEDS TO KNOW IF HITTING VERTICAL OR HORIZONTAL FACE
+	; Set brick state for that brick to 0, and erase the brick
+
+checkBottom:
+	; See if ball hits bottom
+	; Call btouchBot, if r1 = 1 then lose life, reset paddle and ball position, and x,y delta's
+
+checkPaddle:
+	; See if ball hits paddle
+	; Call btouchPaddle, if r1 = 1, call updateBallDeltaForPaddleBounce
+
+checkBounce:
+	; If any of the above happen, update position again using new delta values
+	CMP r4, #0
+	BEQ printBall					; If no touches happen
+	; Recalculate position:
+	LDR r7, ptr_to_xDelta
+	LDRB r7, [r7]
+	LDR r8, ptr_to_yDelta
+	LDRB r8, [r8]					; Load current x and y deltas into r7 and r8
+	ADD r7, r7, r5
+	ADD r8, r8, r6					; Add x and y postions to their respective deltas
+	LDR r9, ptr_to_ballxPosition
+	STRB r7, [r9]
+	LDR r10, ptr_to_ballyPosition
+	LDRB r8, [r10]					; Store new x and y positions to memory
+
+printBall:
+	; Print " " where ball currently is to erase it (this is where we need r5 and r6 unchanged)
+	MOV r0, r5
+	MOV r1, r6
+	BL setCursorxy					; Move cursor to current ball position
+	MOV r0, #0x20
+	BL output_character				; Print a " " character
+
+	; Print the ball in its new position
+	MOV r0, r7
+	MOV r1, r8
+	BL setCursorxy					; Move cursor to new position
+	MOV r0, #0x6F
+	BL output_character				; Print a "o" character
+
+
     ; Update position based on direction stored in current_direction and switch_speed
-    # ldr r7, ptr_to_yposition
-    # ldrbs r9, [r7]
-    # ldr r8, ptr_to_xposition
-    # ldrbs r10, [r8]
-
-	# cmp r9, #0xFF
-	# BEQ game_over_handler
-
-	# cmp r9, #20
-	# BEQ game_over_handler
-
-    # ldr r5, ptr_to_current_direction
-    # LDRB r6, [r5]
-    # cmp r6, #0x77 ; W
-    # IT EQ
-    # ADDEQ r9, r9, #-1 ; y += 1
-    # ;ADDEQ r10, r10, #-1 ; y += 1
-    # cmp r6, #0x61 ; A
-    # IT EQ
-    # ADDEQ r10, r10, #-1 ; x -= 1
-    # cmp r6, #0x73 ; S
-    # IT EQ
-    # ADDEQ r9, r9, #1 ; y -= 1
-    # cmp r6, #0x64 ; D
-	# IT EQ
-	# ADDEQ r10, r10, #1 ; x += 1
-
-    # cmp r10, #0
-	# BEQ game_over_handler
-
-	# cmp r10, #21
-	# BEQ game_over_handler
-
-	# strb r9, [r7]
-	# strb r10, [r8]
-
-	# ldr r7, ptr_to_movement_counter
-	# LDRB r8, [r7]
-	# ADD r8, r8, #1
-	# STRB r8, [r7]
-
-    # ; Call display board to referesh the screen
-    # BL displayboard
 
     ; Restore the registers
     POP {lr, r4-r11}
 
 	BX lr       	; Return
+
 
 escapeSequence:
 	PUSH {lr}
@@ -156,13 +192,22 @@ escapeSequence:
 
 	POP {pc}
 
+
+; Sets the x,y position of the cursor, input r0=x, r1=y
 setCursorxy:
 	PUSH{lr, r4-r11}
 
-	MOV r4, r0 ; x C 0x43
-	ADD r4, r4, #0x30
-	MOV r5, r1 ; y B 0x42
-	ADD r5, r5, #0x31
+	ldr r4, ptr_to_xescapeStringBuffer
+	PUSH {r1}
+	MOV r1, r4
+	BL int2string
+	POP {r1}
+	ldr r5, ptr_to_yescapeStringBuffer
+	PUSH {r0, r1}
+	MOV r0, r1
+	MOV r1, r5
+	POP {r0, r1}
+
 	BL escapeSequence
 	MOV r0, #0x48
 	BL output_character
@@ -171,7 +216,7 @@ setCursorxy:
 	BEQ noxshift
 	BL escapeSequence
 	MOV r0, r4
-	BL output_character
+	BL output_string
 	MOV r0, #0x43 ; column shift for x
 	BL output_character
 noxshift:
@@ -180,7 +225,7 @@ noxshift:
 	BEQ noyshift
 	BL escapeSequence
 	MOV r0, r5
-	BL output_character
+	BL output_string
 	MOV r0, #0x42 ; row shift for y
 	BL output_character
 noyshift:
@@ -188,6 +233,8 @@ noyshift:
 	POP {lr, r4-r11}
 	mov pc, lr
 
+
+; Prints the boundries of the board
 printBoard:
 	; Screen size is 23x19, board edges are 23x18, inner board is 21x16
 	PUSH{lr, r4-r11}
@@ -240,19 +287,20 @@ printBottom:
 	POP {lr, r4-r11}
 	mov pc, lr
 
+
 ; Prints the set amount of blocks in random colors
 displayBricks:
 	PUSH {lr, r4-r11}
 	; Get the brick state
-	mov r0, ptr_to_brickState
-	LDW r4, r0				; Loads brickState into r4
+	ldr r0, ptr_to_brickState
+	LDRW r4, [r0]				; Loads brickState into r4
 
 	; Loop over first 28 bits
 	MOV r5, #0				; Set bit position to be 0
 	MOV r6, #1				; Create a mask
 	; Check bit value
 	AND r7, r5, r6
-	LSR r4
+	; ldr r6, ptr_to_xDeltaLSR r4,
 	CMP r7, r6
 
 
@@ -261,6 +309,7 @@ displayBricks:
 	; Display brick
 
 	; Jump back to loop
+
 
 ; needs to be called before any usage of the btouch methods
 setupForBounceChecks:
@@ -272,6 +321,7 @@ setupForBounceChecks:
 
 	POP {pc}
 
+
 ; return true/false if ball is on a brick
 ; x and y values in r2, r3, value returned in r1
 btouchBrick:
@@ -282,7 +332,8 @@ btouchBrick:
 	CMP r3, #6 ; y > 6
 	BGT btouchBrickfalse ; return false
 	; shift brickState by x*7 places
-	ldrw r1, ptr_to_brickState
+	ldr r1, ptr_to_brickState
+	ldrw r1, [r1]
 	MOV r5, #7
 	MUL r5, r2, r5 ; r5 = x*7
 	LSR r1, r1, r5 ; brickstate >>= r5
@@ -302,9 +353,9 @@ btouchSide:
 	PUSH {lr}
 	; return true if x is 0 or greater than 21
 	CMP r2, #0
-	BEQ btouchsidetrue
+	BEQ btouchSidetrue
 	CMP r2, #21
-	BGT btouchsidetrue
+	BGT btouchSidetrue
 
 	MOV r1, #0 	  ; false
 	POP  {pc}	  ; Restore lr from stack
@@ -313,9 +364,10 @@ btouchSidetrue:
 	MOV r1, #1
 	POP  {pc}	  ; Restore lr from stack
 
+
 ; return true/false if ball is on the top
 ; x and y values in r2, r3, value returned in r1
-btouchtop:
+btouchTop:
 	PUSH {lr}
 	CMP r3, #0
 	ITE LE
@@ -323,9 +375,10 @@ btouchtop:
 	MOVGT r1, #0
 	POP  {pc}
 
+
 ; return true/false if ball is on the bottom
 ; x and y values in r2, r3, value returned in r1
-btouchbot:
+btouchBot:
 	PUSH {lr}
 	CMP r3, #17
 	ITE GE
@@ -333,14 +386,15 @@ btouchbot:
 	MOVLT r1, #0
 	POP  {pc}
 
+
 ; return -1 if ball not on paddle, else the position on the paddle
 ; which it is touching
 ; x and y values and paddlepos in r2, r3, r4 value returned in r1
 btouchPaddle:
 	PUSH {lr}
 	; return true if y position = 16 and x < paddlepos or x > paddlepos+4
-	CMP r3, #16 ; y < 16
-	BLT btouchPaddlefalse ; return false
+	CMP r3, #16 ; y != 16
+	BNE btouchPaddlefalse ; return false
 
 	CMP r2, r4 ; x < paddlepos
 	BLT btouchPaddlefalse ; return false
@@ -352,11 +406,15 @@ btouchPaddle:
 	SUB r1, r2, r4
 	POP  {pc} ; return true
 
+
 btouchPaddlefalse:
 	MOV r1, #-1
 	POP  {pc}
 
+
 ; needs btouch setup to be called before
+; Alternatively, btouchPaddle can return 0 for no touch, 1 for left side, 2 for middle, 3 for right side
+; and based on that value, (xD, yD) = (-1, 1), (0, 1), (1, 1) respectively
 updateBallDeltaForPaddleBounce:
 	PUSH {lr}
 	BL btouchPaddle
@@ -386,7 +444,7 @@ updateBallDeltaForPaddleBounce:
 
 	CMP r1, #1
 	ITT EQ
-	MOVEQ r3, #-2
+	MOVEQ r3, #0xfe
 	BEQ exitUpdateBallDelta
 
 	CMP r1, #0
@@ -395,8 +453,12 @@ updateBallDeltaForPaddleBounce:
 	BEQ exitUpdateBallDelta
 
 exitUpdateBallDelta:
-	str r2, ptr_to_xDelta
-	str r3, ptr_to_yDelta
+	PUSH {r6}
+	ldr r6, ptr_to_xDelta
+	strb r2, [r6]
+	ldr r6, ptr_to_yDelta
+	strb r3, [r6]
+	POP {r6}
 
 exitBallDeltaCheck:
 	POP {pc}
