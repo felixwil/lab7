7dc94fcafb5907b43eec20d15ff4f3ac338db5bf
zemaniack <83049478+zemaniack@users.noreply.github.com>
2023-04-27 17:00:47 -0400

    Update lab7library.s

--- lab7library.s
+++ lab7library.s
@@ -189,90 +189,90 @@ uart_init:
 uart_interrupt_init:
         PUSH {lr, r4-r11}          ; store regs
         ; Configure UART for interrupts
-        
+
         MOV  r11, #0xc038
         MOVT r11, #0x4000          ; setting the address
         LDRW r4, [r11]             ; loading the data into r4
-        
+
         ORR r4, r4, #16             ; r4 |= #8
-        
+
         MOV  r11, #0xc038
         MOVT r11, #0x4000          ; setting the address
         STRW r4, [r11]             ; storing the data from r4
         ; Set processor to allow for interrupts from UART0
-        
+
         MOV  r11, #0xe100
         MOVT r11, #0xe000          ; setting the address
         LDRW r4, [r11]             ; loading the data into r4
-        
+
         ORR r4, r4, #32            ; r4 |= #16
-        
+
         MOV  r11, #0xe100
         MOVT r11, #0xe000          ; setting the address
         STRW r4, [r11]             ; storing the data from r4
-        
+
         POP {lr, r4-r11}           ; restore saved regs
         MOV pc, lr                 ; return to source call
 
 gpio_interrupt_init:
         PUSH {lr, r4-r11}          ; store regs
-        
+
         ; Set interrupt to be edge sensitive
         MOV  r11, #0x5404
         MOVT r11, #0x4002          ; setting the address
         LDRW r4, [r11]             ; loading the data into r4
-        
+
         AND r4, r4, #0xfe          ; r4 &= #0xf7
-        
+
         MOV  r11, #0x5404
         MOVT r11, #0x4002          ; setting the address
         STRW r4, [r11]             ; storing the data from r4
-        
+
         ; Set trigger for interrupt to be single edge
         MOV  r11, #0x5408
         MOVT r11, #0x4002          ; setting the address
         LDRW r4, [r11]             ; loading the data into r4
-        
+
         AND r4, r4, #0xfe          ; r4 &= #0xf7
-        
+
         MOV  r11, #0x5408
         MOVT r11, #0x4002          ; setting the address
         STRW r4, [r11]             ; storing the data from r4
-        
+
         ; Set the falling edge to be the trigger (triggers on press, not release)
         MOV  r11, #0x540c
         MOVT r11, #0x4002          ; setting the address
         LDRW r4, [r11]             ; loading the data into r4
-        
+
         AND r4, r4, #0xfe          ; r4 &= #0xf7
-        
+
         MOV  r11, #0x540c
         MOVT r11, #0x4002          ; setting the address
         STRW r4, [r11]             ; storing the data from r4
-        
+
         ; Enable the the interrupt
         MOV  r11, #0x5410
         MOVT r11, #0x4002          ; setting the address
         LDRW r4, [r11]             ; loading the data into r4
-        
+
         ORR r4, r4, #16          ; r4 |= #0x08
-        
+
         MOV  r11, #0x5410
         MOVT r11, #0x4002          ; setting the address
         STRW r4, [r11]             ; storing the data from r4
-        
-        ; Set processor to allow interrupts from GPIO port F 
+
+        ; Set processor to allow interrupts from GPIO port F
         MOV  r11, #0xe100
         MOVT r11, #0xe000          ; setting the address
         LDRW r4, [r11]             ; loading the data into r4
-        
+
         ORR r4, r4, #0x40000000    ; r4 |= #0x20000000
-        
+
         MOV  r11, #0xe100
         MOVT r11, #0xe000          ; setting the address
         STRW r4, [r11]             ; storing the data from r4
 
-        
+
         POP {lr, r4-r11}           ; restore saved regs
         MOV pc, lr                 ; return to source call             ; return to source call
 
@@ -359,7 +359,7 @@ read_from_push_btns:
         MOV r1, #0x7000
         MOVT r1, #0x4000
         LDRB r0, [r1, #0x3FC]
-        
+
         ; Set a counter and loop variable
         MOV r5, #0      ; counter
         MOV r6, #0      ; loop
@@ -376,7 +376,7 @@ skipPress:
         ADD r6, r6, #1
         CMP r6, #4
         BLT countPressedLoop
-        
+
         ; move the result into return register
         MOV r0, r5
 
